dcc3cc1748003746708a84321377a0921b352f85
DarvanneTapayan <38714205+DarvanneTapayan@users.noreply.github.com>
2023-10-22 19:08:46 +0800

    update

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-age = int(input("Enter your age: "))
+# Python code to run a web server for the calculator
 
-if age >= 19:
-    print("adult")
-elif age >= 13 and age <= 18:
-    print("teenager")
-else:
-    print("child")
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'projects.html'
+        return SimpleHTTPRequestHandler.do_GET(self)
+
+
+if __name__ == '__main__':
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, MyHandler)
+    print('Starting server on port 8000...')
+    httpd.serve_forever()
